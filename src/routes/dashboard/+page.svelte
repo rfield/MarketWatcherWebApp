5a06ed87e61ad63c5a48112926fbbb2a58cfa9e2
Richard J. Field <rjfield@mac.lan>
2026-02-20 11:53:32 -0500

    get prices for assets

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,6 +5,7 @@
 <script>
   import { currentUser } from '../stores.js';
   import { currentAssets } from '../stores.js';
+  import { _toDollars } from '../utils.js';
 
   let { data } = $props();
 </script>
@@ -14,7 +15,7 @@
 <table>
   <thead>
     <tr>
-	  <th>Portfolio</th>
+	    <th>Portfolio</th>
       <th>Ticker</th>
       <th>Price</th>
       <th>Units</th>
@@ -25,14 +26,23 @@
     <!-- Loop through the data -->
     {#each $currentAssets as asset}
       <tr>
-        <td>{asset.accountName}</td>
+        <td style="background-color: #b3ecf5; font-style: italic;">{asset.accountName}</td>
         <td>{asset.ticker}</td>
         <td>{asset.price}</td>
         <td>{asset.holdingAmount}</td>
-        <td>{asset.total}</td>
+        <td style="text-align: right;">{asset.total}</td>
       </tr>
     {/each}
   </tbody>
+  <tfoot>
+    <tr>
+      <td style="font-weight: bold;">Total</td>
+      <td colspan="4" style="font-weight: bold; text-align: right;">
+        {_toDollars($currentAssets.reduce((sum, asset) => sum + (asset.holdingAmount * asset.price), 0))}
+        <!--${$currentAssets.reduce((sum, asset) => sum + (asset.holdingAmount * asset.price), 0).toFixed(2)}-->
+      </td>
+    </tr>
+  </tfoot>
 </table>
 
 <p></p>
@@ -41,5 +51,5 @@
 <style>
   table { width: 50%; border-collapse: collapse; }
   th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
-  th { background-color: #f2f2f2; }
+  th, tfoot td { background-color: #f2f2f2; }
 </style>
